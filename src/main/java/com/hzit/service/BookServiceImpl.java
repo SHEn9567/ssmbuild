b6cad92cdd70c12b1e28a8b7240ee2c6bf867016
SHEn9567 <2351903481@qq.com>
2020-08-13 22:03:34 +0800

    Initial commit

---
+++ src/main/java/com/hzit/service/BookServiceImpl.java
@@ -0,0 +1,36 @@
+package com.hzit.service;
+
+import com.hzit.dao.BookMapper;
+import com.hzit.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    //Service调dao层:组合Dao
+    //调用dao层的操作，设置一个set接口，方便Spring管理
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
